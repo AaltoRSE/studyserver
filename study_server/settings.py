f1930842f553f19dc79e772509ec4daca754c750
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2025-09-26 11:24:15 +0300

    Use porgresql

--- study_server/settings.py
+++ study_server/settings.py
@@ -9,18 +9,21 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p@jdu6$ugool=+$#lan3&9fyfgy3k5+%vv8owj19zgsl)4^*-y'
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-p@jdu6$ugool=+$#lan3&9fyfgy3k5+%vv8owj19zgsl)4^*-y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +78,9 @@ WSGI_APPLICATION = 'study_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
+
 
 
 # Password validation
