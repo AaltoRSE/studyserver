952c4080b439153716571cde9e005e75977bf5e0
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2025-10-16 20:48:15 +0300

    Add imported local settings file

--- study_server/settings.py
+++ study_server/settings.py
@@ -13,6 +13,11 @@ import environ
 from pathlib import Path
 import os
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-p@jdu6$ugool=+$#lan3&9fy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -149,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
